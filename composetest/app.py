a3901864684414b5b7e8cb7317185de530218e53
nzgamer41 <pslover9@gmail.com>
2020-06-21 22:19:53 +1200

    question 4

--- composetest/app.py
+++ composetest/app.py
@@ -2,6 +2,7 @@ import time
 
 import redis
 from flask import Flask
+from math import sqrt; from itertools import count, islice
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
@@ -22,7 +23,7 @@ def store_number(number):
     retries = 5
     while True: 
         try:
-            return cache.append("primes", number)
+            return cache.append("primes", number + ",")
         except redis.exceptions.ConnectionError as exc:
             if retries == 0:
                 raise exc
@@ -30,15 +31,23 @@ def store_number(number):
             time.sleep(0.5)
 
 def get_primes():
-    retries = 5
-    while True: 
-        try:
-            return cache.get("primes")
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+    try:
+        retries = 5
+        while True: 
+            try:
+                return cache.get("primes")
+            except redis.exceptions.ConnectionError as exc:
+                if retries == 0:
+                    raise exc
+                retries -= 1
+                time.sleep(0.5)
+    except:
+        return "no primes"
+
+@app.route('/debugging/reset')
+def clear():
+    cache.delete("primes")
+    return "reset cache"
 
 
 @app.route('/')
@@ -48,24 +57,22 @@ def hello():
 
 @app.route('/isPrime/<numberVar>')
 def isPrime(numberVar):
+    n = int(numberVar)
     try:
-        number = int(numberVar)
-        if number > 1:
-            for i in range(2, number):
-                if (number % i) == 0:
-                    return '{} is not prime'.format(number)
-                    break
-            else:
-                store_number(numberVar + ",")
-                return '{} is prime'.format(number)
-
+        prime = n > 1 and all(n%i for i in islice(count(2), int(sqrt(n)-1)))
+        if prime == True:
+            store_number(numberVar)
+            return '{} is prime'.format(n)
         else:
-            return '{} is not prime'.format(number)
+            return '{} is not prime'.format(n)
     except ValueError:
         return 'ERROR: Value must be a decimal number!'
 
 @app.route('/primesStored')
 def primesStored():
-    values = get_primes()
-    return values
+    try:
+        values = get_primes()
+        return values
+    except:
+        return "No primes are stored!"
     
